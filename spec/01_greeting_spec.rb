84bc1685312f5eca30a8506a3ff41d13a7c26132
granit <graniitkastrati@gmail.com>
2019-10-22 15:02:01 +0000

    Automatically backed up by Learn

--- spec/01_greeting_spec.rb
+++ spec/01_greeting_spec.rb
@@ -12,18 +12,18 @@ describe './lib/greeting.rb' do
       expect{greeting("Bobby")}.to_not raise_error
     end
 
-  #  it 'greets the person whose name was passed in as an argument' do
-  #    allow($stdout).to receive(:puts)
-#
-#      output = capture_puts{ greeting("Sally") }
+    it 'greets the person whose name was passed in as an argument' do
+      allow($stdout).to receive(:puts)
 
-#      expect(output).to include("Hello Sally. It's nice to meet you.")
-#    end
+      output = capture_puts{ greeting("Sally") }
 
-  #  it 'greets the person with additional whitespace removed' do
-  #    allow($stdout).to receive(:puts)
+      expect(output).to include("Hello Sally\t    . It's nice to meet you.")
+    end
 
-  #    output = capture_puts{ greeting(" Sally\t   ") }
+    it 'greets the person with additional whitespace removed' do
+      allow($stdout).to receive(:puts)
+
+      output = capture_puts{ greeting(" Sally\t   ") }
 
       # puts adds a newline to the output. Without using print, students
       # will not be able to remove the \n character at the end of the phrase
